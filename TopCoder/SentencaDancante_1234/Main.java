0d1d688e0592fcb995ac843e35e1e74e76f7a505
Lucas Capistrano dos Santos <capistrano.lucas@hotmail.com>
2016-07-27 11:53:54 -0300

    Update Main.java

--- TopCoder/SentencaDancante_1234/Main.java
+++ TopCoder/SentencaDancante_1234/Main.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package SentencaDancante_1234;
 
 import java.util.Scanner;
@@ -30,36 +29,3 @@ public class Main {
 				}
 		}
 }
-=======
-package SentencaDancante_1234;
-
-import java.util.Scanner;
-
-public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		while(sc.hasNext()){
-			
-			String s = sc.nextLine();
-			StringBuilder stringAux = new StringBuilder();	
-			int x = 2;
-			
-			for (int i = 0; i < s.length(); i++) {
-				if(s.charAt(i) != ' '){
-					if(x % 2 == 0){
-						stringAux.append(s.toUpperCase().charAt(i));
-					}else{
-						stringAux.append(s.toLowerCase().charAt(i));
-					}
-					x++;
-				}else{
-					stringAux.append(s.charAt(i));
-				}	
-			}
-			System.out.println(stringAux);
-				
-				}
-		}
-}
->>>>>>> origin/master
